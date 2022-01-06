4bd037a6169cd6a91200b50fd1b7201e3f8bdaf9
Robilko <sasha.robson@gmail.com>
2022-01-07 01:20:33 +0300

    Урок 6. Добавлен room. Согласно задания - при первом обращении записываем ответ с сервера в БД. При нажатии на элемент списка на втором экране отображаем поля данного юзера. Данные для второго экрана согласно задания  берем не из сети, а из БД.

--- homework5/src/main/java/com/example/homework5/App.kt
+++ homework5/src/main/java/com/example/homework5/App.kt
@@ -1,10 +1,21 @@
 package com.example.homework5
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.content.Context
 import com.example.homework5.navigation.CustomRouter
 import com.github.terrakok.cicerone.Cicerone
 
 class App: Application() {
+
+    @SuppressLint("StaticFieldLeak")
+    object ContextHolder { lateinit var context: Context }
+
+
+    override fun onCreate() {
+        super.onCreate()
+        ContextHolder.context = this
+    }
 
     companion object Navigation {
 
--- homework5/src/main/java/com/example/homework5/data/GitHubUserRepositoryImpl.kt
+++ homework5/src/main/java/com/example/homework5/data/GitHubUserRepositoryImpl.kt
@@ -1,13 +1,30 @@
 package com.example.homework5.data
 
 import com.example.homework5.data.retrofit.GitHubApiFactory
+import com.example.homework5.data.room.RoomFactory
 import io.reactivex.rxjava3.core.Single
 
 class GitHubUserRepositoryImpl : GitHubUserRepository {
 
     private val gitHubApi = GitHubApiFactory.create()
+    private val roomDb = RoomFactory.create().getGitHubUserDao()
 
-    override fun getUsers(): Single<List<GitHubUser>> = gitHubApi.fetchUsers()
+    override fun getUsers(): Single<List<GitHubUser>> {
+        return roomDb.getUsers()
+            .flatMap {
+                if (it.isEmpty()) {
+                    gitHubApi.fetchUsers()
+                        .map { resultFromServer ->
+                            roomDb.saveUser(resultFromServer)
+                            resultFromServer
+                        }
+                } else {
+                    Single.just(it)
+                }
+            }
+    }
 
-    override fun getUserByLogin(login: String): Single<GitHubUser> = gitHubApi.fetchUserByLogin(login)
+    override fun getUserByLogin(login: String): Single<GitHubUser> {
+        return roomDb.getUserByLogin(login)
+    }
 }
